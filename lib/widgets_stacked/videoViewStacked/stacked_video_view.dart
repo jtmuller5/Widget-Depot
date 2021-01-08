5cb9e9173bfb76dc477ae1e58a54bc3016f52944
jtmuller5 <47997351+jtmuller5@users.noreply.github.com>
2021-01-08 00:36:39 -0500

    Setup

---
+++ lib/widgets_stacked/videoViewStacked/stacked_video_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:widget_depot/widgets_stacked/videoViewStacked/stacked_video_view_model.dart';
+
+class StackedVideoView extends ViewModelWidget<StackedVideoViewModel> {
+
+  @override
+  Widget build(BuildContext context, StackedVideoViewModel model) {
+    return Container();
+  }
+
+}
